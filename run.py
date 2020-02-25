e3f2034531121247f9c903e738ed73f8c0213966
woodfordtim <woodfordtim@gmail.com>
2020-02-25 22:43:24 +0000

    Refactoring to use chat.html - not yet finished

--- run.py
+++ run.py
@@ -11,12 +11,7 @@ def add_messages(username, message):
     """Add messages to the 'messages' list"""
     now = datetime.now().strftime("%H:%M:%S")
     messages_dict = {"timestamp": now, "from": username, "message": message}
-
-    messages.append("({}) {}: {}". format(now, username, message))
-
-def get_all_messages():
-    """Get all of the message and separate them with a 'br' """
-    return "<br>".join(messages)
+    messages.append(messages_dict)
 
 @app.route('/', methods = ["GET", "POST"])
 def index():
@@ -33,7 +28,7 @@ def index():
 @app.route('/<username>')
 def user(username):
     """Display chat messages"""
-    return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages())
+    return "<h1>Welcome, {0}</h1>{1}".format(username, messages)
 
 @app.route('/<username>/<message>')
 def send_message(username, message):
